b6267d6bddc85dfa9332965eebcc75e7b642759c
Yuvinca Peña <yuvinca.flores@gmail.com>
2013-06-08 12:47:48 -0600

    nueva clase para mostrar piesas

---
+++ src/jueguitos/Piesas.java
@@ -0,0 +1,18 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package jueguitos;
+
+/**
+ *
+ * @author Yuvinca
+ */
+public class Piesas {
+    
+    public void piesas (){
+        System.out.print("mostrar piesas");
+        System.out.print("mostrar piesas");
+    }
+    
+}
